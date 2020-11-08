004601862f08ce7d34e73948931176fe39fb7230
Nur Sasongko <32429721+ssasongko@users.noreply.github.com>
2020-11-08 13:18:04 +0700

    Add files via upload

---
+++ PBOIF2-10119049-Latihan46-TandanyaKamu/src/Human/Age.java
@@ -0,0 +1,65 @@
+package Human;
+/**
+ * @author
+ * Nama  : Nur Sasongko
+ * Kelas : PBOIF2
+ * NIM   : 10119049
+ * Deskripsi Program : Program meramal umur
+ */
+public class Age {
+    private int yearBirth;
+    private int yearNow;
+    private String red = "\u001B[31m";
+    
+    public void Age(int yearNow){
+        this.yearNow = yearNow;
+    }
+    
+    public void setYearBirth(int yearBirth){
+        this.yearBirth = yearBirth;
+    }
+    
+    public int getYearBirth(){
+        return yearBirth;
+    }
+    
+    public int getYearNow(){
+        return yearNow;
+    }
+    
+    public int hitungUmur(){
+        return yearNow - yearBirth;
+    }
+    
+    public String tandanyaKamu(int umur){
+        String pesan = "";
+        
+        if(0 <= umur && umur <= 5) {
+            pesan = "LAGI LUCU-LUCUNYA";
+        }
+        else if(5 < umur && umur <= 10) {
+            pesan = "MASIH ANAK-ANAK";
+        }
+        else if(10 < umur && umur <= 13) {
+            pesan = "MASIH REMADJA";
+        }
+        else if(13 < umur && umur <= 19) {
+            pesan = "ALAY";
+        }
+        else if(19 < umur && umur <= 29) {
+            pesan = "LAGI GALAU NYARI JODOH";
+        }
+        else if(29 < umur && umur <= 35) {
+            pesan = "LAGI SIBUK NYARI UANG";
+        }
+        else if(35 < umur && umur <= 150) {
+            pesan = "SUDAH TUA";
+        }
+        else{
+            pesan = "TIDAK TERDETEKSI DI KEHIDUPAN";
+        }
+        return red + pesan;
+    }
+    
+    
+}
---
+++ PBOIF2-10119049-Latihan46-TandanyaKamu/src/pboif2/pkg10119049/latihan46/tandanyakamu/PBOIF210119049Latihan46TandanyaKamu.java
@@ -0,0 +1,29 @@
+package pboif2.pkg10119049.latihan46.tandanyakamu;
+/**
+ * @author
+ * Nama  : Nur Sasongko
+ * Kelas : PBOIF2
+ * NIM   : 10119049
+ * Deskripsi Program : Program meramal umur
+ */
+import java.util.Scanner;
+import Human.Age;
+import java.util.Calendar;
+public class PBOIF210119049Latihan46TandanyaKamu {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Age umur = new Age();
+        
+        System.out.printf("Masukkan Tahun Lahir Anda : ");umur.setYearBirth(scan.nextInt());
+        
+        System.out.printf("\n======Hasil Perhitungan Umur======\n");
+        System.out.printf("Tahun lahir anda : %d\n",umur.getYearBirth());
+        umur.Age(Calendar.getInstance().get(Calendar.YEAR));
+        System.out.printf("Hari ini tahun : %d\n",umur.getYearNow());
+        System.out.printf("Umur kamu sampai hari ini adalah %d tahun\n", umur.hitungUmur());
+        System.out.printf("Tandanya Kamu %s\n", umur.tandanyaKamu(umur.hitungUmur()));
+        
+        
+    }
+    
+}
